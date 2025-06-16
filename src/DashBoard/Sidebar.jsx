24eb7b1e4d5d86ef1372181afd4669b20330332d
rayhan <mohammadrayhanx0@gmail.com>
2025-06-16 21:20:25 +0600

    responsive dashboard

--- src/DashBoard/Sidebar.jsx
+++ src/DashBoard/Sidebar.jsx
@@ -6,7 +6,9 @@ const Sidebar = () => {
     return (
         <div className="w-64 bg-white border-r px-4 py-6 hidden lg:block">
             <nav className="space-y-4">
-                <Link to="add-marathon" className="block text-blue-600 hover:underline">Add Marathon</Link>
+                <div>
+                    <Link to="add-marathon" className="block text-blue-600 hover:underline">Add Marathon</Link>
+                </div>
                 <Link to="my-marathons" className="block text-blue-600 hover:underline">My Marathon List</Link>
                 <Link to="/apply-list" className="block text-blue-600 hover:underline">My Apply List</Link>
             </nav>
--- src/Layout/DashBoardLayout.jsx
+++ src/Layout/DashBoardLayout.jsx
@@ -1,28 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router';
 import Sidebar from '../DashBoard/Sidebar';
 import Navbar from '../Header/Navbar';
 import Footer from '../Header/Footer';
+import { FaBars } from "react-icons/fa";
+
 
 const DashBoardLayout = () => {
-    return (
-        <div className="min-h-screen flex flex-col">
-            <Navbar></Navbar>
-            <div className="flex">
-                {/* Sidebar */}
-                <aside className='sticky  top-0 h-fit'>
-                    <Sidebar />
-                </aside>
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-                {/* Main Content */}
-                <main className="flex-1 p-4 bg-gray-100">
-                    <Outlet />
-                </main>
-            </div>
+  return (
+    <div className="min-h-screen flex flex-col">
+      {/* Top Navbar */}
+      <Navbar />
 
-            <Footer />
-        </div>
-    );
+      {/* Mobile Menu Button */}
+      <div className="lg:hidden bg-white p-4 shadow flex items-center justify-between">
+        <button
+          onClick={() => setIsSidebarOpen(true)}
+          className="text-2xl text-gray-700"
+        >
+          <FaBars />
+        </button>
+        <h1 className="text-lg font-bold">Dashboard</h1>
+      </div>
+
+      <div className="flex flex-1 relative">
+        {/* Sidebar Overlay for Mobile */}
+        {isSidebarOpen && (
+          <div
+            className="fixed inset-0 bg-black bg-opacity-40 z-40 lg:hidden"
+            onClick={() => setIsSidebarOpen(false)}
+          ></div>
+        )}
+
+        {/* Sidebar */}
+        <aside
+          className={`fixed top-0 left-0 h-full w-64 bg-white shadow-lg z-50 transform transition-transform duration-300 ease-in-out
+          ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"} 
+          lg:translate-x-0 lg:static lg:block`}
+        >
+          <Sidebar onLinkClick={() => setIsSidebarOpen(false)} />
+        </aside>
+
+        {/* Main Content */}
+        <main className="flex-1 p-4 bg-gray-100 overflow-x-hidden">
+          <Outlet />
+        </main>
+      </div>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
 };
 
 export default DashBoardLayout;
